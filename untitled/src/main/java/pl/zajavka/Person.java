11ef67bb25a3072aa7213d58fe3d9173b5999827
LukaSz90 <lukasz90sz@gmail.com>
2024-03-01 12:00:28 +0100

    class: add Person class and simple person generator

---
+++ untitled/src/main/java/pl/zajavka/Person.java
@@ -0,0 +1,9 @@
+package pl.zajavka;
+
+public class Person {
+    private String name;
+    private String surname;
+    private int counter;
+
+
+}
---
+++ untitled/src/main/java/pl/zajavka/Process.java
@@ -0,0 +1,12 @@
+package pl.zajavka;
+
+public class Process {
+
+    //No VIP (<imie>) + person VIP (<imie>, VIP)
+    private final String ADD_PERSON = "ADD PERSON";
+    private final String PROCESS = "PROCESS";
+    private final String LEAVE_PERSON = "LEAVE PERSON";
+
+
+
+}
