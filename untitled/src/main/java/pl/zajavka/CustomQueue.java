7d4eb51662a76b60b099aa957dae64d8deb0e281
LukaSz90 <lukasz90sz@gmail.com>
2024-03-10 19:09:37 +0100

    CustomQueue class: add function enter and leave in quque

--- untitled/src/main/java/pl/zajavka/CustomQueue.java
+++ untitled/src/main/java/pl/zajavka/CustomQueue.java
@@ -36,15 +36,34 @@ public class CustomQueue<T> {
     }
 
     public void enterIntoStore() {
+        if (customerQueue.isEmpty()) {
+            System.out.println("No item in the queue.");
+        } else {
+            handleEnterByQueue(customerQueueVIP);
+            return;
+        }
 
+        T itemEntered = customerQueue.poll();
+
+        System.out.printf("Processing queue: %s arrived at the store", itemEntered);
+        System.out.println(customerQueue);
+        System.out.println();
     }
 
     private void handleEnterByQueue(Deque<T> customerQueueVIP) {
-
+        T itemEntered = customerQueueVIP.poll();
+        System.out.printf("Processing queue: %s arrived at the store", itemEntered);
     }
 
     public void leave(T item) {
-
+        if (customerQueue.contains(item)) {
+            customerQueue.remove(item);
+            System.out.printf("Leaving queue: %s", item);
+            System.out.println(customerQueue);
+            System.out.println();
+        } else {
+            System.out.printf(item + " currently is not in the queue.\n");
+        }
     }
 
 
