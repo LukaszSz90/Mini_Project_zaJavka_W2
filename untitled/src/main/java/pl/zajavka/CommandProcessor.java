2671c0179f60dcee0086f52e9f365967fdffb411
LukaSz90 <lukasz90sz@gmail.com>
2024-03-11 18:52:41 +0100

    CommandProcessor.class : create class and add required method

--- untitled/src/main/java/pl/zajavka/CommandProcessor.java
+++ untitled/src/main/java/pl/zajavka/CommandProcessor.java
@@ -15,15 +15,47 @@ public class CommandProcessor {
         } else if (command.contains("LEAVE")) {
             leavePerson(command);
         } else if (command.contains("PROCESS")) {
-
+            processPerson(command);
         } else {
             throw new RuntimeException("Unknown command");
+        }
+    }
+
+    private void addIncomePerson(String command, boolean isVip) {
+        System.out.println(command);
+        Person incomePerson = createPerson(command, isVip);
+
+        if (isVip) {
+            queueCommand.joinToQueueVip(incomePerson);
+        } else {
+            queueCommand.joinToQueue(incomePerson);
         }
     }
 
     private void leavePerson(String command) {
         System.out.println(command);
         Person leavingPerson = createLeavingPerson(command);
+        queueCommand.leaveQueue(leavingPerson);
+    }
+
+    private void processPerson(String command) {
+        System.out.println(command);
+        queueCommand.enterToStore();
+    }
+
+
+    private Person createPerson(String command, boolean vip) {
+        String replaceCommand = command
+                .replace("ADD PERSON(", "")
+                .replace(vip ? ",VIP)" : ")", "");
+
+        String[] splitData = replaceCommand.split("_");
+
+        if (splitData.length == 2) {
+            return new Person(splitData[0], splitData[1], queueCommand.checkAndIncrementCount(command));
+        } else {
+            throw new IllegalArgumentException("Illegal argument: " + command);
+        }
     }
 
     private Person createLeavingPerson(String command) {
@@ -37,32 +69,6 @@ public class CommandProcessor {
             return new Person(splitData[0], splitData[1], 1);
         } else if (splitData.length == 3) {
             return new Person(splitData[0], splitData[1], Integer.parseInt(splitData[2]));
-        } else {
-            throw new IllegalArgumentException("Illegal argument: " + command);
-        }
-    }
-
-    private void addIncomePerson(String command, boolean isVip) {
-
-        System.out.println(command);
-        Person incomePerson = createPerson(command, isVip);
-
-        if (isVip) {
-            queueCommand.joinToQueueVip(incomePerson);
-        } else {
-            queueCommand.joinToQueue(incomePerson);
-        }
-    }
-
-    private Person createPerson(String command, boolean vip) {
-        String replaceCommand = command
-                .replace("ADD PERSON(", "")
-                .replace(vip ? ",VIP)" : ")", "");
-
-        String[] splitData = replaceCommand.split("_");
-
-        if (splitData.length == 2) {
-            return new Person(splitData[0], splitData[1], queueCommand.checkAndIncrementCount(command));
         } else {
             throw new IllegalArgumentException("Illegal argument: " + command);
         }
