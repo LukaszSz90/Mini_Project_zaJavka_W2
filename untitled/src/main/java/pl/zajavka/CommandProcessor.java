3ce334528382ce04f1c2a6f9b950414647be79d0
LukaSz90 <lukasz90sz@gmail.com>
2024-03-11 18:42:56 +0100

    QueueCommand.class : create class and add required method

---
+++ untitled/src/main/java/pl/zajavka/CommandProcessor.java
@@ -0,0 +1,70 @@
+package pl.zajavka;
+
+import javax.swing.text.html.ListView;
+
+public class CommandProcessor {
+    private QueueCommand<Person> queueCommand;
+
+    public CommandProcessor(QueueCommand<Person> queueCommand) {
+        this.queueCommand = queueCommand;
+    }
+
+    public void processCommand(String command) {
+        if (command.contains("ADD PERSON")) {
+            addIncomePerson(command, command.contains("VIP"));
+        } else if (command.contains("LEAVE")) {
+            leavePerson(command);
+        } else if (command.contains("PROCESS")) {
+
+        } else {
+            throw new RuntimeException("Unknown command");
+        }
+    }
+
+    private void leavePerson(String command) {
+        System.out.println(command);
+        Person leavingPerson = createLeavingPerson(command);
+    }
+
+    private Person createLeavingPerson(String command) {
+        String replaceCommand = command
+                .replace("LEAVE PERSON(", "")
+                .replace(")", "");
+
+        String[] splitData = replaceCommand.split("_");
+
+        if(splitData.length == 2) {
+            return new Person(splitData[0], splitData[1], 1);
+        } else if (splitData.length == 3) {
+            return new Person(splitData[0], splitData[1], Integer.parseInt(splitData[2]));
+        } else {
+            throw new IllegalArgumentException("Illegal argument: " + command);
+        }
+    }
+
+    private void addIncomePerson(String command, boolean isVip) {
+
+        System.out.println(command);
+        Person incomePerson = createPerson(command, isVip);
+
+        if (isVip) {
+            queueCommand.joinToQueueVip(incomePerson);
+        } else {
+            queueCommand.joinToQueue(incomePerson);
+        }
+    }
+
+    private Person createPerson(String command, boolean vip) {
+        String replaceCommand = command
+                .replace("ADD PERSON(", "")
+                .replace(vip ? ",VIP)" : ")", "");
+
+        String[] splitData = replaceCommand.split("_");
+
+        if (splitData.length == 2) {
+            return new Person(splitData[0], splitData[1], queueCommand.checkAndIncrementCount(command));
+        } else {
+            throw new IllegalArgumentException("Illegal argument: " + command);
+        }
+    }
+}
--- untitled/src/main/java/pl/zajavka/QueueCommand.java
+++ untitled/src/main/java/pl/zajavka/QueueCommand.java
@@ -9,37 +9,63 @@ public class QueueCommand<T> {
     private Deque<T> queueVIP;
     private Deque<T> queue;
 
-    private Map<String,Integer> countCustomer = new HashMap<>();
+    private Map<String,Integer> countCustomerMap = new HashMap<>();
 
     public QueueCommand(Deque<T> queueVIP, Deque<T> queue) {
         this.queueVIP = queueVIP;
         this.queue = queue;
     }
 
+    public void joinToQueueVip(T element) {
+        boolean isAdded = queueVIP.offer(element);
+        System.out.printf("%s came to the queue: %s%n", element, isAdded);
+        System.out.println(queue);
+        System.out.println();
+    }
+
     public void joinToQueue(T element) {
         boolean isAdded = queue.offer(element);
-
-    }
-
-    public void joinToQueueVip() {
-
-    }
-
-    public void enterToStore() {
-
-    }
-
-    public void leaveQueue() {
-
-    }
-
-    private void enterToStoreVip() {
-
+        System.out.printf("%s came to the queue: %s%n", element, isAdded);
+        System.out.println(queue);
+        System.out.println();
     }
 
     public Integer checkAndIncrementCount(String elementKey) {
-
-        return 1;
+        Integer count = countCustomerMap.getOrDefault(elementKey, 0);
+        countCustomerMap.put(elementKey, ++count);
+        return count;
     }
+
+    public void enterToStore() {
+        if (queue.isEmpty()) {
+            System.out.println("Queue is empty");
+        } else {
+            enterToStoreVip(queueVIP);
+            return;
+        }
+
+        T enteredElement = queue.poll();
+
+        System.out.printf("Processing queue: %s arrived at store", enteredElement);
+        System.out.println(queue);
+        System.out.println();
+    }
+
+    private void enterToStoreVip(Deque<T> queueVIP) {
+        T enteredElement = queueVIP.poll();
+        System.out.printf("Processing queue: %s arrived at the store", enteredElement);
+    }
+
+    public void leaveQueue(T item) {
+        if (queue.contains(item)) {
+            queue.remove(item);
+            System.out.println("Leaving queue: " + item);
+            System.out.println(queue);
+            System.out.println();
+        } else {
+            System.out.println(item + " is not currently in queue.");
+        }
+    }
+
 
 }
